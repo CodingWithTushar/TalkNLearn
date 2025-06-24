461ad0ee52f0cb2de58554bf56704a260416a6fa
Tushar <156307843+CodingWithTushar@users.noreply.github.com>
2025-06-24 22:48:47 +0530

    Deployment Error

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,9 +30,9 @@ app.use("/api/v1/chat", ChatRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 }
 
 app.listen(port, () => {
